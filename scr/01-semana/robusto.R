9dddbf6464227eab205bca420942065a51fcf512
Sara Acevedo <30844164+Saryace@users.noreply.github.com>
2025-09-04 21:52:04 -0400

    add clase 03

--- scr/01-semana/robusto.R
+++ scr/01-semana/robusto.R
@@ -24,7 +24,7 @@ saludo_animal <-
 
 saludo_animal(
   nombre = "Sara",
-  animal = "lagartija"
+  animal = "random"
 )
 
 # Nueva funcion -----------------------------------------------------------
